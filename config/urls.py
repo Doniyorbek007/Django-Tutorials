421faf2ff99596789991307c053e6fc1ffc68189
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-07-10 17:45:28 +0300

    done

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,7 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", include("hello_world.urls")),
-    path("admin/", admin.site.urls),
-=======
     path("admin/", admin.site.urls),
     path("", include("hello_world.urls")),
->>>>>>> c86398c99ef9df716ca541b0b3e20b4bb9f5456a
 ]
--- hello_world/views.py
+++ hello_world/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello World!")
+def home(request):
+    return HttpResponse("<h1>Home</h1>")
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
