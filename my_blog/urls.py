c0aa7af46777b39ebd130ff949a7f50b1ea701d5
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-07-12 18:12:08 +0300

    ...

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hello, name="hello"),
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("about/",views.about, name="about"),
     path("my_blog/",views.my_blog, name="my_blog"),
     path("porfolio/",views.porfolio, name="portfolio"),
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,22 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse("<h1>Hello World!</h1><div><a href='/home'style='margin-right:10px;'>Home</a><a href='/about'style='margin:10px;'>About</a><a href='/my_blog' style='margin:10px;'>My blog</a><a href='/porfolio'style='margin:10px;'>Portfolio</a><a href='/contact'style='margin:10px;'>Contact</a></div>")
+def home(req):
+    return render(req, "index.html")
 
-def home(request):
-    return HttpResponse("<h1>Home</h1><div><a href='/home'style='margin-right:10px;'>Home</a><a href='/about'style='margin:10px;'>About</a><a href='/my_blog' style='margin:10px;'>My blog</a><a href='/porfolio'style='margin:10px;'>Portfolio</a><a href='/contact'style='margin:10px;'>Contact</a></div>")
+def blog(req):
+    return render(req,"blog.html")
 
-def about(request):
-    return HttpResponse("<h1>About</h1><div><a href='/home'style='margin-right:10px;'>Home</a><a href='/about'style='margin:10px;'>About</a><a href='/my_blog' style='margin:10px;'>My blog</a><a href='/porfolio'style='margin:10px;'>Portfolio</a><a href='/contact'style='margin:10px;'>Contact</a></div>")
+def about(req):
+    return render(req,"about.html")
 
-def my_blog(request):
-    return HttpResponse("<h1>My blog</h1><div><a href='/home'style='margin-right:10px;'>Home</a><a href='/about'style='margin:10px;'>About</a><a href='/my_blog' style='margin:10px;'>My blog</a><a href='/porfolio'style='margin:10px;'>Portfolio</a><a href='/contact'style='margin:10px;'>Contact</a></div>")
 
-def porfolio(request):
-    return HttpResponse("<h1>Porfolio</h1><div><a href='/home'style='margin-right:10px;'>Home</a><a href='/about'style='margin:10px;'>About</a><a href='/my_blog' style='margin:10px;'>My blog</a><a href='/porfolio'style='margin:10px;'>Portfolio</a><a href='/contact'style='margin:10px;'>Contact</a></div>")
 
-def contact(request):
-    return HttpResponse("<h1>Contact</h1><div><a href='/home'style='margin-right:10px;'>Home</a><a href='/about'style='margin:10px;'>About</a><a href='/my_blog' style='margin:10px;'>My blog</a><a href='/porfolio'style='margin:10px;'>Portfolio</a><a href='/contact'style='margin:10px;'>Contact</a></div>")
+def contact(req):
+    return render(req, "contact.html")
