0dca6a53b81393e8e1677bf9c69e8abbc894d42d
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-07-17 16:47:23 +0500

    done

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about/",views.about, name="about"),
-    path("blog/",views.blog, name="blog"),
-    path("portfolio/",views.portfolio, name="portfolio"),
-    path("contact/",views.contact, name="contact"),
+    path("about/", views.about, name="about"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contact, name="contact"),
+
 ]
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
 # from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(req):
-    context = {'name':'Sardor'} 
+    # return HttpResponse("<h1>Home</h1>")
+    context = {'name': 'Sardor'}
     return render(req, "pages/home.html", context)
 
-def blog(req):
-    return render(req,"pages/blog.html")
-
 def about(req):
-    return render(req,"pages/about.html")
+    return render(req, "pages/about.html")
 
 def portfolio(req):
     return render(req, "pages/portfolio.html")
+
+def blog(req):
+    return render(req, "pages/blog.html")
 
 def contact(req):
     return render(req, "pages/contact.html")
