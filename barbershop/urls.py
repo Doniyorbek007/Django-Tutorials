0da1a262f775dac960e3cff94d4b29454168d464
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-07-19 12:23:03 +0500

    done

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.base, name="base"),
+    path("home/", views.home, name="home"),
+    path("our-story/", views.our_story, name="our-story"),
+    path("services/", views.sevices, name="services"),
+    path("price-list/", views.price_list, name="price-list"),
+    path("contact/", views.contact, name="contact"),
 ]
--- barbershop/views.py
+++ barbershop/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(req):
+def base(req):
     return render(req, 'base.html')
+
+def home(req):
+    return render(req, 'pages/section1.html')
+
+def our_story(req):
+    return render(req, 'pages/section2.html')
+
+def sevices(req):
+    return render(req, 'pages/section3.html')
+
+def price_list(req):
+    return render(req, 'pages/section4.html')
+
+def contact(req):
+    return render(req, 'pages/section5.html')
