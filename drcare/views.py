11fe0c5fe6b5c684d05f125646bd65b4a542dfad
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-07-22 20:25:33 +0500

    done

--- drcare/views.py
+++ drcare/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req,'base.html')
+
+def about(req):
+    return render(req, 'pages/about.html')
+
+def doctor(req):
+    return render(req, 'pages/doctor.html')
+
+def departments(req):
+    return render (req, 'pages/departments.html')
+
+def pricing(req):
+    return render(req, 'pages/pricing.html')
+
+def blog(req):
+    return render(req, 'pages/blog.html')
+
+def contact(req):
+    return render(req, 'pages/contact.html')
