04f3da1eecfac1c577a1de3cb1f125bda7dd38ee
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-09-08 17:36:53 +0300

    done

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'user.apps.UserConfig',
+    'file.apps.FileConfig',
+    # 'user.apps.UserConfig',
     # 'paginator.apps.PaginatorConfig',
     # 'todo.apps.TodoConfig',
     # 'forms.apps.FormsConfig',
@@ -128,12 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
-MEDIA_ROOT = BASE_DIR / 'data/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
+
 MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,9 @@ from django.conf.urls.static import serve
 urlpatterns = [
     path("admin/", admin.site.urls),
     
-    path('',include('user.urls')),
+    path('',include('file.urls')),
+    
+    # path('',include('user.urls')),
 
     # path('',include('paginator.urls')),
    
